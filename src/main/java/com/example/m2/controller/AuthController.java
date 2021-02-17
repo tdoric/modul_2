14d5c882303e50a06da6f44c89469db905e139b5
Tomislav Dorić <tdoric@intranet.fina.hr>
2021-02-17 22:18:33 +0100

    Login part

---
+++ src/main/java/com/example/m2/controller/AuthController.java
@@ -0,0 +1,59 @@
+package com.example.m2.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.m2.jwt.JwtUtils;
+import com.example.m2.request.LoginRequest;
+import com.example.m2.response.JwtResponse;
+import com.example.m2.services.UserDetailsImpl;
+
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+	
+	@Autowired
+	AuthenticationManager authenticationManager;
+
+	@Autowired
+	JwtUtils jwtUtils;
+
+	@PostMapping("/signin")
+	public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = jwtUtils.generateJwtToken(authentication);
+		
+		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();		
+		List<String> roles = userDetails.getAuthorities().stream()
+				.map(item -> item.getAuthority())
+				.collect(Collectors.toList());
+
+		return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getId(), 
+				userDetails.getUsername(),  userDetails.getEmail(), roles));
+				
+				
+				
+				
+			
+	}
+
+
+}
+
